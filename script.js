b31613460235ad38807caaccfe9a0f1c6fc5821a
Rosalie-Baxter <rosalie.baxter@outlook.com>
2019-10-22 21:36:16 +0100

    assigned previous key type custom attribute

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const calculator = document.querySelector(".calculator")
 const keys = calculator.querySelector(".calculator-keys")
 const currentNumber = document.querySelector('.current-operand')
+const previousKeyType = calculator.dataset.previousKeyType
 
 keys.addEventListener("click", e => {
   if (e.target.matches("button")) {
@@ -11,7 +12,7 @@ keys.addEventListener("click", e => {
     Array.from(key.parentNode.children)
    .forEach(k => k.classList.remove('is-depressed'))
   if (!action) {
-    if (displayedNum==="0") {
+    if (displayedNum ==="0"|| previousKeyType === "operator") {
       currentNumber.textContent = keyContent;
     } else {
       currentNumber.textContent = displayedNum + keyContent;
@@ -23,6 +24,7 @@ keys.addEventListener("click", e => {
     action === "divide"
   ) {
   key.classList.add("is-depressed")
+  calculator.dataset.previousKeyType = 'operator'
   } if (action==="clear") {
     console.log("clear!")
   }  if (action==="delete") {
