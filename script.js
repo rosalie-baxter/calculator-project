6aedcc629dc3528b6afcf42b422967ad6112c1a5
Rosalie-Baxter <rosalie.baxter@outlook.com>
2019-10-23 13:19:59 +0100

    adjusted previouskeytype dataset to fix multiple digit bug in second value

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const calculator = document.querySelector(".calculator")
 const keys = calculator.querySelector(".calculator-keys")
 const currentNumber = document.querySelector('.current-operand')
-const previousKeyType = calculator.dataset.previousKeyType
+
+const calculate = (n1, operator, n2) => {
+  let result = ''
+  if (operator === 'add') {
+    result = parseFloat(n1) + parseFloat(n2)
+  } else if (operator === 'subtract') {
+    result = parseFloat(n1) - parseFloat(n2)
+  } else if (operator === 'multiply') {
+    result = parseFloat(n1) * parseFloat(n2)
+  } else if (operator === 'divide') {
+    result = parseFloat(n1) / parseFloat(n2)
+  }
+  return result
+}
 
 keys.addEventListener("click", e => {
   if (e.target.matches("button")) {
@@ -9,10 +22,12 @@ keys.addEventListener("click", e => {
     const action = key.dataset.action
     const keyContent = key.textContent
     const displayedNum = currentNumber.textContent
+    const previousKeyType = calculator.dataset.previousKeyType
     Array.from(key.parentNode.children)
    .forEach(k => k.classList.remove('is-depressed'))
   if (!action) {
-    if (displayedNum ==="0"|| previousKeyType === "operator") {
+    calculator.dataset.previousKeyType = "numberkey"
+    if (displayedNum === "0"|| previousKeyType === "operator") {
       currentNumber.textContent = keyContent;
     } else {
       currentNumber.textContent = displayedNum + keyContent;
@@ -24,13 +39,18 @@ keys.addEventListener("click", e => {
     action === "divide"
   ) {
   key.classList.add("is-depressed")
-  calculator.dataset.previousKeyType = 'operator'
+  calculator.dataset.previousKeyType = "operator"
+  calculator.dataset.firstValue = displayedNum
+  calculator.dataset.operator = action
   } if (action==="clear") {
     console.log("clear!")
   }  if (action==="delete") {
     console.log("delete!")
   } if (action==="calculate") {
-    console.log("equals!")
+  const firstValue = calculator.dataset.firstValue
+  const operator = calculator.dataset.operator
+  const secondValue = displayedNum
+  currentNumber.textContent = calculate(firstValue, operator, secondValue)
   }
   }
 })
